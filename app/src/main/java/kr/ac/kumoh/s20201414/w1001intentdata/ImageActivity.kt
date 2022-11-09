52bdab3be6cf7833c16df8099800a351fcb475f2
jkc-mycode <jkc9709@naver.com>
2022-11-09 15:07:55 +0900

    :tada: 좋아요 싫어요 구현

--- app/src/main/java/kr/ac/kumoh/s20201414/w1001intentdata/ImageActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201414/w1001intentdata/ImageActivity.kt
@@ -1,12 +1,24 @@
 package kr.ac.kumoh.s20201414.w1001intentdata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import android.widget.Toast
 import kr.ac.kumoh.s20201414.w1001intentdata.databinding.ActivityImageBinding
 
-class ImageActivity : AppCompatActivity() {
+class ImageActivity : AppCompatActivity(), OnClickListener {
+    companion object {
+        const val LIKE = 10
+        const val DISLIKE = 20
+        const val NONE = 0
+
+        const val imageName = "image"
+        const val resultName = "result"
+    }
     private lateinit var binding: ActivityImageBinding
+    private var image: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityImageBinding.inflate(layoutInflater)
@@ -14,14 +26,28 @@ class ImageActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         //Toast.makeText(this, intent.getStringExtra("image"), Toast.LENGTH_LONG).show()
-
-
-        val res = when(intent.getStringExtra("image")){
+        image = intent.getStringExtra(MainActivity.keyName)
+        val res = when(image){
             "jellyfish" -> R.drawable.jellyfish
             "star" -> R.drawable.star
             else -> R.drawable.ic_launcher_background
         }
-
         binding.imgGundam.setImageResource(res)
+
+        binding.btnLike.setOnClickListener(this)
+        binding.btnDislike.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent()
+        val value = when (v?.id){
+            binding.btnLike.id -> LIKE
+            binding.btnDislike.id -> DISLIKE
+            else -> NONE
+        }
+        intent.putExtra(imageName, image)
+        intent.putExtra(resultName, value)
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
--- app/src/main/java/kr/ac/kumoh/s20201414/w1001intentdata/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20201414/w1001intentdata/MainActivity.kt
@@ -1,28 +1,59 @@
 package kr.ac.kumoh.s20201414.w1001intentdata
 
 import android.content.Intent
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.ac.kumoh.s20201414.w1001intentdata.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnClickListener {
+    companion object {
+        const val keyName = "image"
+    }
+
     private lateinit var binding: ActivityMainBinding
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         //setContentView(R.layout.activity_main)
         setContentView(binding.root)
 
-        binding.btnGundam.setOnClickListener{
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra("image", "jellyfish")
-            startActivity(intent)
-        }
+        binding.btnGundam.setOnClickListener(this)
+        binding.btnZaku.setOnClickListener(this)
 
-        binding.btnZaku.setOnClickListener{
-            val intent = Intent(this, ImageActivity::class.java)
-            intent.putExtra("image", "star")
-            startActivity(intent)
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            if(it.resultCode != RESULT_OK)
+                return@registerForActivityResult
+
+            val result = it.data?.getIntExtra(ImageActivity.resultName, ImageActivity.NONE)
+            val str = when (result) {
+                ImageActivity.LIKE -> "좋아요"
+                ImageActivity.DISLIKE -> "싫어요"
+                else -> ""
+            }
+            val image = it.data?.getStringExtra(ImageActivity.imageName)
+
+            when (image) {
+                "jellyfish" -> binding.btnGundam.text = "건담 ($str)"
+                "star" -> binding.btnZaku.text = "자쿠 ($str)"
+            }
         }
+    }
+
+    override fun onClick(v: View?) {
+        val intent = Intent(this, ImageActivity::class.java)
+        val value = when (v?.id){
+            binding.btnGundam.id -> "jellyfish"
+            binding.btnZaku.id -> "star"
+            else -> return
+        }
+        intent.putExtra(keyName, value)
+        launcher.launch(intent)
     }
 }
